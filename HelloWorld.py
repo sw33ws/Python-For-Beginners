e329b631565c85175d46217b5d5287b40c561a4f
sw33ws <gillisl22@outlook.com>
2023-02-26 11:22:55 -0500

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,75 @@
+# red_bucket = input("what is the cat doing? ")
+# print(red_bucket)
+
+
+# Break
+# cat_age = 5
+# other_cat_age = 5
+
+# if cat_age < other_cat_age:
+#     print("other cats older!")
+# elif cat_age == other_cat_age:
+#     print("both cats are the same age!")
+# else:
+#     print("cat is older!")
+
+
+# Break
+# def print_cat():
+#     text = "cats are cool"
+#     print(text)
+
+# print_cat()
+
+
+# Break
+# def print_cat(text):
+#     print(text)
+
+# print_cat("cats are cool yoo")
+
+# Break
+# def school_age_calculator(age,name): 
+#     if age < 5:
+#         print("no need for school!", name, "is only", age)
+#     elif age == 5:
+#         print("enjoy starting school,", name)
+#     else:
+#         print("They grow up so fast!")
+
+# school_age_calculator(10, "Bob")
+
+
+# Break
+# def add_ten_to_age(age):
+#     new_age = age + 10
+#     return new_age
+
+# how_old_will_i_be = add_ten_to_age(3)
+# print(how_old_will_i_be)
+
+
+# Break
+# x=0
+# while (x<5):
+#     print(x)
+#     x=x+1
+
+
+# Break
+# for x in range(5,10):
+#     print(x)
+
+
+# Break
+# days =["Mon","Tue","Wed","Thu","Fri", "Sat", "Sun"]
+
+# for d in days:
+#     if(d=="Thu"):continue
+#     # changing continue to break will stop the loop
+#     print(d)
+
+
+# Break
+# import math
+# print("pi is", math.pi)
